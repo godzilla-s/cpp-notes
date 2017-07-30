a3a9975189cb02604f2f8ef4b7e0c0f28fb7f34f
Zhuweijin87 <zhuweijin87@github.com>
2017-07-30 03:54:28 -0700

    update

--- 01_strategy.cc
+++ 01_strategy.cc
@@ -7,8 +7,8 @@ using std::endl;
 class IStrategy
 {
 public:
-    IStrategy(void);
-    virtual ~IStrategy(void);
+    IStrategy(void) {;}
+    virtual ~IStrategy(void) {;}
     virtual void Operate(void) = 0;
 };
 
@@ -36,25 +36,32 @@ void CContext::Operate(void)
     this->m_pStrategy->Operate();
 }
 
+//继承策略
 class CBackDoor : public IStrategy
 {
 public:
     CBackDoor(void);
     ~CBackDoor(void);
+	//实现父类方法(一定)
     void Operate(void);
 };
 
 CBackDoor::CBackDoor(void)
 {
+
 }
+
 CBackDoor::~CBackDoor(void)
 {
+
 }
+
 void CBackDoor::Operate(void)
 {
     cout << "找乔国老帮忙，让吴国太给孙权施加压力" << endl;
 }
 
+//继承策略父类
 class CGivenGreenLight : public IStrategy
 {
 public:
@@ -65,10 +72,14 @@ public:
 
 CGivenGreenLight::CGivenGreenLight(void)
 {
+
 }
+
 CGivenGreenLight::~CGivenGreenLight(void)
 {
+
 }
+
 void CGivenGreenLight::Operate(void)
 {
     cout << "求吴国太开个绿灯，放行！" << endl;
@@ -84,10 +95,12 @@ public:
 
 CBlockEnemy::CBlockEnemy(void)
 {
+
 }
 
 CBlockEnemy::~CBlockEnemy(void)
 {
+
 }
 
 void CBlockEnemy::Operate()
@@ -99,25 +112,22 @@ int main()
 {
     CContext *pContext;
 
-    cout << "\14\n\n\n\n\17" << endl;
     cout << "----------刚刚到吴国的时候拆第一个----------" << endl;
     pContext = new CContext(new CBackDoor());
     pContext->Operate();
     delete pContext;
 
-    cout << "\14\n\n\n\n\17" << endl;
     cout << "----------刘备乐不思蜀了，拆第二个了----------" << endl;
     pContext = new CContext(new CGivenGreenLight());
     pContext->Operate();
     delete pContext;
 
-    cout << "\14\n\n\n\n\17" << endl;
     cout << "----------孙权的小兵追了，咋办？拆第三个----------" << endl;
     pContext = new CContext(new CBlockEnemy());
     pContext->Operate();
     delete pContext;
 
-    _CrtSetDbgFlag(_CRTDBG_LEAK_CHECK_DF | _CRTDBG_ALLOC_MEM_DF);
-    _CrtDumpMemoryLeaks();
+    //_CrtSetDbgFlag(_CRTDBG_LEAK_CHECK_DF | _CRTDBG_ALLOC_MEM_DF);
+    //_CrtDumpMemoryLeaks();
     return 0;   
 }
