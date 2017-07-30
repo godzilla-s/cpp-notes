f59704fa22260ee3cf97b403e5e2f0f75894daf8
Zhuweijin87 <zhuweijin87@github.com>
2017-07-30 04:03:12 -0700

    update and compile 02

--- 02_proxy.cc
+++ 02_proxy.cc
@@ -7,8 +7,8 @@ using namespace std;
 class IKindWomen
 {
 public:
-    IKindWomen(void);
-    virtual ~IKindWomen(void);
+    IKindWomen(void) {;}
+    virtual ~IKindWomen(void) {;}
     virtual void MakeEyesWithMan() = 0;
     virtual void HappyWithMan() = 0;
 };
