cd213b5a3f30bfba4b3c4e5a1191846402960516
Weijin Zhu <472214158@qq.com>
2017-08-02 13:49:46 +0800

    Update 08_adapter.cc

--- 08_adapter.cc
+++ 08_adapter.cc
@@ -1,5 +1,6 @@
 //适配器
 #include <iostream>
+#include <string>
 
 unsing namespace std;
 
@@ -143,3 +144,146 @@ public:
         return "0";
     }
 };
+
+class COuterUserHomeInfo
+{
+public:
+    COuterUserHomeInfo(void)
+    {
+    }
+    ~COuterUserHomeInfo(void)
+    {
+    }
+    string GetHomeAddress()
+    {
+        cout << "这里是员工的家庭地址..." << endl;
+        return "0";
+    }
+    string GetHomeTelNumber()
+    {
+        cout << "员工的家庭电话是..." << endl;
+        return "0";
+    }
+};
+
+class COuterUserOfficeInfo
+{
+public:
+    COuterUserOfficeInfo(void)
+    {
+    }
+    ~COuterUserOfficeInfo(void)
+    {
+    }
+    string GetOfficeTelNumber()
+    {
+        cout << "办公室电话是..." << endl;
+        return "0";
+    }
+    string GetJobPosition()
+    {
+        cout << "这个人的职位是BOSS..." << endl;
+        return "0";
+    }
+};
+
+class COuterUserInfo : public IUserInfo
+{
+public:
+    COuterUserInfo(void);
+    ~COuterUserInfo(void);
+    string GetUserName();
+    string GetHomeAddress();
+    string GetMobileNumber();
+    string GetOfficeTelNumber();
+    string GetJobPosition();
+    string GetHomeTelNumber();
+private:
+    COuterUser *m_pOuterUser;
+};
+
+COuterUserInfo::COuterUserInfo(void)
+{
+    m_pOuterUser = new COuterUser();
+}
+COuterUserInfo::~COuterUserInfo(void)
+{
+    delete m_pOuterUser;
+}
+string COuterUserInfo::GetUserName()
+{
+    COuterUserBaseInfo *pBaseInfo = m_pOuterUser->GetUserBaseInfo();
+    pBaseInfo->GetUserName();
+    delete pBaseInfo;
+    pBaseInfo = NULL;
+    return "0";
+}
+string COuterUserInfo::GetHomeAddress()
+{
+    COuterUserHomeInfo *pHomeInfo = m_pOuterUser->GetUserHomeInfo();
+    pHomeInfo->GetHomeAddress();
+    delete pHomeInfo;
+    pHomeInfo = NULL;
+    return "0";
+}
+string COuterUserInfo::GetMobileNumber()
+{
+    COuterUserBaseInfo *pBaseInfo = m_pOuterUser->GetUserBaseInfo();
+    pBaseInfo->GetMobileNumber();
+    delete pBaseInfo;
+    pBaseInfo = NULL;
+    return "0";
+}
+string COuterUserInfo::GetOfficeTelNumber()
+{
+    COuterUserOfficeInfo *pOfficeInfo = m_pOuterUser->GetUserOfficeInfo();
+    pOfficeInfo->GetOfficeTelNumber();
+    delete pOfficeInfo;
+    pOfficeInfo = NULL;
+    return "0";
+}
+string COuterUserInfo::GetJobPosition()
+{
+    COuterUserOfficeInfo *pOfficeInfo = m_pOuterUser->GetUserOfficeInfo();
+    pOfficeInfo->GetJobPosition();
+    delete pOfficeInfo;
+    pOfficeInfo = NULL;
+    return "0";
+}
+string COuterUserInfo::GetHomeTelNumber()
+{
+    COuterUserHomeInfo *pHomeInfo = m_pOuterUser->GetUserHomeInfo();
+    pHomeInfo->GetHomeTelNumber();
+    delete pHomeInfo;
+    pHomeInfo = NULL;
+    return "0";
+}
+
+void DoIt()
+{
+    IUserInfo *pYourGirl = new CUserInfo();
+    for(int i = 0; i < 101; i += 20)
+    {
+        pYourGirl->GetMobileNumber();
+    }
+    delete pYourGirl;
+}
+void NowDoIt()
+{
+    IUserInfo *pYourGirl = new COuterUserInfo();
+
+    for(int i = 0; i < 101; i += 20)
+    {
+        pYourGirl->GetMobileNumber();
+    }
+
+    delete pYourGirl;
+}
+int main(int argc, char* argv[])
+{
+    DoIt();
+    
+    NowDoIt();
+    
+    return 0;
+}
